24c269b27c351e3979ff089debd463b1fbb4a7a7
HingC <102840461+HingC@users.noreply.github.com>
2023-05-23 02:12:31 +0900

    Update server_start.sh

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-sudo chmod +x /root/tomcat/bin/./startup.sh
-sudo /root/tomcat/bin/./startup.sh
+## sudo chmod +x /root/tomcat/bin/./startup.sh
+## sudo /root/tomcat/bin/./startup.sh
+
+sudo systemctl restart httpd.service
